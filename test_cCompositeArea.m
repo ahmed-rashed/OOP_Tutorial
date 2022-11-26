bc46ea664dfba532e7c7ac2bd605a9e858ac65d2
Ahmed Rashed <ahmed.m.rashed@gmail.com>
2022-11-26 13:04:02 +0200

    Update

--- test_cCompositeArea.m
+++ test_cCompositeArea.m
@@ -1,22 +1,23 @@
 clc
-clear all %#ok<*CLALL>
+clearvars
+
+% All properties are in mm
 
 % Problem 1.2-e
-A_vec=[1450;2610;1450;4810;1450;2610;1450]*1e-6;
-Iy_vec=[0.278;0.633;0.278;51.2;0.278;0.633;0.278]*1e-6;
-Iz_vec=[0.799;15;0.799;2.8;0.799;15;0.799]*1e-6;
-Iyz_1=-tan(2*(atan(.413)+pi/2))*(Iy_vec(1)-Iz_vec(1))/2;
-Iyz_vec=[Iyz_1;0;-Iyz_1;0;-Iyz_1;0;Iyz_1];
+A1=1450;Iy1=0.278;Iz1=0.799;Iyz1=-tan(2*(atan(.413)+pi/2))*(Iy1-Iz1)/2;y1=(203/2+27.4);z1=(254/2+7.7-14.7);
+A2=2610;Iy2=0.633;Iz2=15;Iyz2=0;y2=0;z2=254/2+7.7-14.1;
+A4=4810;Iy4=51.2;Iz4=2.8;Iyz4=0;y4=0;z4=0;
 
-y1=203e-3/2+27.4e-3;
-z1=254e-3/2+7.7e-3-14.7e-3;
-z2=254e-3/2+7.7e-3-14.1e-3;
-y_hat_vec=[y1;0;-y1;0;y1;0;-y1];
-z_hat_vec=[z1;z2;z1;0;-z1;-z2;-z1];
+A_vec=[A1,A2,A1,A4,A1,A2,A1];
+Iy_vec=[Iy1,Iy2,Iy1,Iy4,Iy1,Iy2,Iy1];
+Iz_vec=[Iz1,Iz2,Iz1,Iz4,Iz1,Iz2,Iz1];
+Iyz_vec=[Iyz1,Iyz2,-Iyz1,Iyz4,-Iyz1,-Iyz2,Iyz1];
+y_hat_vec=[y1,y2,-y1,y4,y1,-y2,-y1];
+z_hat_vec=[z1,z2,z1,z4,-z1,-z2,-z1];
 
-oSec1=cCompositeArea(A_vec,Iy_vec,Iz_vec,Iyz_vec,y_hat_vec,z_hat_vec) %#ok<*NOPTS>
+oSec1=cCompositeArea(A_vec,Iy_vec,Iz_vec,Iyz_vec,y_hat_vec,z_hat_vec);
 
-A=oSec1.A
+A=oSec1.A %#ok<*NOPTS> 
 y_bar=oSec1.y_bar
 z_bar=oSec1.z_bar
 Iy=oSec1.Iy
